307aab1fa87dce3b68b5f219561027801e7ab2dc
Theobhg <assis.theo@gmail.com>
2020-04-23 17:35:12 -0300

    add state/click button-x

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+        >
+        {this.state.value}
       </button>
     );
   }
@@ -10,7 +25,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
 
@@ -60,5 +75,5 @@ class Game extends React.Component {
 
 ReactDOM.render(
   <Game />,
-  document.geElementById('root')
+  document.getElementById('root')
 );
